689872a24dd805854454a8ae06eec8005fa8f7cc
yhames <wjddn494@naver.com>
2023-06-15 15:10:52 +0900

    NamedParameterJdbcTemplate

--- src/main/java/project/finance/repository/ExpenditureRepositoryJdbc.java
+++ src/main/java/project/finance/repository/ExpenditureRepositoryJdbc.java
@@ -2,8 +2,12 @@ package project.finance.repository;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import project.finance.category.ExpenditureCategory;
@@ -15,45 +19,41 @@ import javax.sql.DataSource;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+// TODO : NamedParameterJdbcTemplate, SimpleJdbcInsert 추가
 @Slf4j
 public class ExpenditureRepositoryJdbc implements ExpenditureRepository {
 
-    private final JdbcTemplate template;
+    //    private final JdbcTemplate template;
+    private final NamedParameterJdbcTemplate template;
 
     public ExpenditureRepositoryJdbc(DataSource dataSource) {
-        this.template = new JdbcTemplate(dataSource);
+        this.template = new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Override
     public void save(Expenditure expenditure) {
-        String sql = "insert into expenditure(category, title, detail, value, date, payment) values (?,?,?,?,?,?)";
-        // 자동 증가 키
+        String sql = "insert into expenditure(category, title, detail, value, date, payment)" +
+                " values (:category,:title,:detail,:value,:date,:payment)";
+        BeanPropertySqlParameterSource param = new BeanPropertySqlParameterSource(expenditure);
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        template.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, expenditure.getCategory().toString());
-            ps.setString(2, expenditure.getTitle());
-            ps.setString(3, expenditure.getDetail());
-            ps.setLong(4, expenditure.getValue());
-            ps.setDate(5, Date.valueOf(expenditure.getDate()));
-            ps.setString(6, expenditure.getPayment().toString());
-            return ps;
-        }, keyHolder);
+        template.update(sql, param, keyHolder);
     }
 
     @Override
     public void update(Long id, ExpenditureUpdate updateParam) {
-        String sql = "update expenditure set category=?, title=?, detail=?, value=?, date=?, payment=? where id=?";
-        template.update(sql,
-                updateParam.getCategory(),
-                updateParam.getTitle(),
-                updateParam.getDetail(),
-                updateParam.getValue(),
-                updateParam.getDate(),
-                updateParam.getPayment(),
-                id);
+        String sql = "update expenditure" +
+                " set category=:category, title=:title, detail=:detail, value=:value, date=:date, payment=:payment where id=:id";
+        MapSqlParameterSource param = new MapSqlParameterSource()
+                .addValue("category", updateParam.getCategory())
+                .addValue("title", updateParam.getTitle())
+                .addValue("detail", updateParam.getDetail())
+                .addValue("value", updateParam.getValue())
+                .addValue("payment", updateParam.getPayment())
+                .addValue("id", id);    // 이 부분 때문에 MapSqlParameterSource 사용
+        template.update(sql, param);
     }
 
     @Override
@@ -64,9 +64,12 @@ public class ExpenditureRepositoryJdbc implements ExpenditureRepository {
 
     @Override
     public Optional<Expenditure> findById(Long id) {
-        String sql = "select category, title, detail, value, date, payment from expenditure where id=?";
+        String sql = "select category, title, detail, value, date, payment from expenditure where id=:id";
+        Map<String, Object> param = Map.of("id", id);
         try {
-            Expenditure expenditure = template.queryForObject(sql, expenditureRowMapper(), id);
+            // 이름 지정 파라미터가 1개여서 가장 단순한 Map 사용
+            // `param`은 2nd 매개변수로 전달
+            Expenditure expenditure = template.queryForObject(sql, param, expenditureRowMapper());
             return Optional.of(expenditure);
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
@@ -74,16 +77,6 @@ public class ExpenditureRepositoryJdbc implements ExpenditureRepository {
     }
 
     private RowMapper<Expenditure> expenditureRowMapper() {
-        return ((rs, num) -> {
-            Expenditure expenditure = Expenditure.builder()
-                    .category(ExpenditureCategory.valueOf(rs.getString("category")))
-                    .title(rs.getString("title"))
-                    .detail(rs.getString("detail"))
-                    .value(rs.getLong("value"))
-                    .date(rs.getDate("date").toLocalDate())
-                    .payment(PaymentType.valueOf(rs.getString("payment")))
-                    .build();
-            return expenditure;
-        });
+        return BeanPropertyRowMapper.newInstance(Expenditure.class);
     }
 }
